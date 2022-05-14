ec4893ca3b267550fcd7f94ab60253c5af010394
Daniel Maricic <daniel@woss.io>
2022-05-14 22:12:31 +0000

    env files and operation repos
    Signed-off-by: Daniel Maricic <daniel@woss.io>

--- .devcontainer/install-deps.sh
+++ .devcontainer/install-deps.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
 echo "Installing the Anagolay CLI"
-sudo sh -c 'curl https://ipfs.anagolay.network/ipfs/bafybeia4ggdxxmrif357p6inwbuvzoxsavj24ol7h2cuvcnnnng7zgfqsi > /usr/local/bin/anagolay && chmod +x /usr/local/bin/anagolay'
+sudo sh -c 'curl https://bafybeiawp434qdc6slnxn4dssvzorywjg2xvp3lojuw25y6fna3z2vq3py.ipfs.anagolay.network > /usr/local/bin/anagolay && chmod +x /usr/local/bin/anagolay'
 
 # smoke test
 echo "Anagolay version is $(anagolay --version)"
 
-if [ ! -d "/workspace/op-file" ]; then
-	echo "Cloning the Op-File to the root for testing"
-	git clone https://gitlab.com/anagolay/op-file.git 
+if [ ! -d "/workspace/op_file" ]; then
+	echo "Cloning the Op_File to the root for testing"
+	git clone https://gitlab.com/anagolay/operations/op_file.git 
 fi
+
+if [ ! -d "/workspace/op_multihash" ]; then
+	echo "Cloning the Op-Multihash to the root for testing"
+	git clone https://gitlab.com/anagolay/operations/op_multihash.git 
+fi
+
+if [ ! -d "/workspace/op_cid" ]; then
+	echo "Cloning the Op-cid to the root for testing"
+	git clone https://gitlab.com/anagolay/operations/op_cid.git 
+fi
+
+
