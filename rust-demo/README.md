8ee2a6c21f427988a2d88438565abb93a8858fc8
adriano.dalpane <adriano.dalpane@gmail.com>
2022-05-17 18:37:12 +0200

    rust workflow demo

--- rust-demo/README.md
+++ rust-demo/README.md
@@ -1,10 +1,19 @@
 # rust-demo
 
-Demonstrate execution of op_file operation
+Demonstrate execution of the wf_file_multihash_cid workflow. This workflow computes the CID of a 
+file given its URL. Retrieved by an HTTP call, the content of the file is hashed using Blake3 and 
+a CID v1 is computed afterwards.
+
 The main function accepts an argument which is the URL of the file to fetch. If no argument is passed,
 a default URL is used
 
 ## Usage
+
+To build:
+
+```sh
+cargo build
+```
 
 To fetch the default url:
 
--- rust-demo/src/main.rs
+++ rust-demo/src/main.rs
@@ -1,27 +1,44 @@
-use std::{collections::BTreeMap, env, time::Instant};
+use core::any::Any;
+use std::env;
+use std::rc::Rc;
+use wf_file_multihash_cid::Workflow;
 
 #[tokio::main]
 async fn main() {
-  let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
-  // If no arguments are supplied, use default URL. Otherwise, use first argument
-  let file_url = if args.len() == 1 {
-    String::from("https://ipfs.anagolay.network/ipfs/bafybeiavjzfgrxx2zq5r3vx352amhuzdv5pc5cu32xp7tlh4iqvcuxjcze/tenerife-light-painting-01-1000x1000.jpg")
-  } else {
-    args[1].clone()
-  };
-  println!("File is {}", &file_url);
+    // If no arguments are supplied, use default URL. Otherwise, use first argument
+    let (file_url, is_default_url) = if args.len() == 1 {
+        (String::from("https://ipfs.anagolay.network/ipfs/bafybeiavjzfgrxx2zq5r3vx352amhuzdv5pc5cu32xp7tlh4iqvcuxjcze/tenerife-light-painting-01-1000x1000.jpg"), true)
+    } else {
+        (args[1].clone(), false)
+    };
+    println!("File is {}", &file_url);
 
-  // Measure performance
-  let now = Instant::now();
+    // Instance the workflow
+    let workflow = Workflow::new();
 
-  // Await operation execution. No configuration needed
-  let config = BTreeMap::new();
-  let output = op_file::execute(&file_url, config).await.unwrap();
+    // Run the next segment of the workflow
+    let inputs: Vec<Rc<dyn Any>> = vec![Rc::new(file_url)];
+    let result = workflow.next(inputs).await.unwrap();
+    let result = result.as_ref();
 
-  let exec_time = now.elapsed().as_millis() as usize;
+    // Extract the output and some performance statistics
+    assert!(result.is_done(), "Workflow is not finished");
+    let output = result
+        .get_output()
+        .unwrap()
+        .downcast_ref::<String>()
+        .unwrap()
+        .clone();
+    let total_time = result.get_total_time();
 
-  // Output some statistics
-  println!("Execution time is: {} millis", exec_time);
-  println!("Operation output size: {:?} bytes", output.decode().len());
+    if is_default_url {
+        assert_eq!(
+            "bafkr4ih2xmsije6aa6yfwjdfmztnnkbb6ip56g3ojfcyfgjx6jsh6bogoe", output,
+            "Wrong CID calculated"
+        );
+    }
+    println!("Execution time is: {} millis", total_time);
+    println!("Workflow computed CID: {}", output);
 }
