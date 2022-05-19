0b2ad0570b29a8b70d1d6818323f879e7e74f511
Daniel Maricic <daniel@woss.io>
2022-05-19 10:01:04 +0000

    git clone issue fixed and some devops
    Signed-off-by: Daniel Maricic <daniel@woss.io>

--- .devcontainer/install-deps.sh
+++ .devcontainer/install-deps.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
+
+
 echo "Installing the Anagolay CLI"
 sudo sh -c 'curl https://bafybeiawp434qdc6slnxn4dssvzorywjg2xvp3lojuw25y6fna3z2vq3py.ipfs.anagolay.network > /usr/local/bin/anagolay && chmod +x /usr/local/bin/anagolay'
 
@@ -8,19 +11,28 @@ echo "Anagolay version is $(anagolay --version)"
 
 if [ ! -d "/workspace/operations/op_file" ]; then
 	echo "Cloning the Op_File to the root for testing"
-	git clone https://gitlab.com/anagolay/operations/op_file.git /workspace/operations 
+	git clone https://gitlab.com/anagolay/operations/op_file.git /workspace/operations/op_file
 fi
 
 if [ ! -d "/workspace/operations/op_multihash" ]; then
 	echo "Cloning the Op-Multihash to the root for testing"
-	git clone https://gitlab.com/anagolay/operations/op_multihash.git /workspace/operations 
+	git clone https://gitlab.com/anagolay/operations/op_multihash.git /workspace/operations/op_multihash 
 fi
 
 if [ ! -d "/workspace/operations/op_cid" ]; then
 	echo "Cloning the Op-cid to the root for testing"
-	git clone https://gitlab.com/anagolay/operations/op_cid.git /workspace/operations 
+	git clone https://gitlab.com/anagolay/operations/op_cid.git /workspace/operations/op_cid 
 fi
 
 # install pnpm
 echo "Installing pnpm"
 npm install -g pnpm
+
+echo "Installing nodejs demo deps"
+cd $PROJECT_ROOT/demos/node-demo 
+pnpm install
+
+
+echo "Installing rust demo deps"
+cd $PROJECT_ROOT/demos/rust-demo 
+cargo fetch
