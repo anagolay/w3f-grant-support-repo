107ca3b9958b1de76557975a7ce72b5c7af4d056
Daniel Maricic <daniel@woss.io>
2022-05-21 12:20:05 +0000

    devops
    Signed-off-by: Daniel Maricic <daniel@woss.io>

--- .devcontainer/install-deps.sh
+++ .devcontainer/install-deps.sh
@@ -8,7 +8,7 @@ sudo apt update && sudo apt install -y direnv
 
 
 echo "Installing the Anagolay CLI"
-sudo sh -c 'curl https://bafybeievibptoefxtwzgfjj2ki6hi4zurvoyrxcoialmv3qgjodx655roy.ipfs.anagolay.network > /usr/local/bin/anagolay && chmod +x /usr/local/bin/anagolay'
+sudo sh -c 'curl https://bafybeicjepyfpfqqesjyuqf2zlebww3kzplscmzp4cqjdl5pwqiriadh4u.ipfs.anagolay.network > /usr/local/bin/anagolay && chmod +x /usr/local/bin/anagolay'
 
 # smoke test
 echo "Anagolay version is $(anagolay --version)"
@@ -18,30 +18,21 @@ if [ ! -f "$HOME/.bashrc.d/custom" ]; then
 	ln -sf $PROJECT_ROOT/.devcontainer/custom_bashrc $HOME/.bashrc.d/custom 
 fi
 
-if [ ! -d "/workspace/operations/op_file" ]; then
+if [ ! -d "$PROJECT_ROOT/operations/op_file" ]; then
 	echo "Cloning the Op_File to the root for testing"
-	git clone https://gitlab.com/anagolay/operations/op_file.git /workspace/operations/op_file
+	git clone https://gitlab.com/anagolay/operations/op_file.git $PROJECT_ROOT/operations/op_file
 fi
 
-if [ ! -d "/workspace/operations/op_multihash" ]; then
+if [ ! -d "$PROJECT_ROOT/operations/op_multihash" ]; then
 	echo "Cloning the Op-Multihash to the root for testing"
-	git clone https://gitlab.com/anagolay/operations/op_multihash.git /workspace/operations/op_multihash 
+	git clone https://gitlab.com/anagolay/operations/op_multihash.git $PROJECT_ROOT/operations/op_multihash 
 fi
 
-if [ ! -d "/workspace/operations/op_cid" ]; then
+if [ ! -d "$PROJECT_ROOT/operations/op_cid" ]; then
 	echo "Cloning the Op-cid to the root for testing"
-	git clone https://gitlab.com/anagolay/operations/op_cid.git /workspace/operations/op_cid 
+	git clone https://gitlab.com/anagolay/operations/op_cid.git $PROJECT_ROOT/operations/op_cid 
 fi
 
 # install pnpm
 echo "Installing pnpm"
 npm install -g pnpm
-
-# echo "Installing nodejs demo deps"
-# cd $PROJECT_ROOT/demos/nodejs
-# pnpm install
-
-
-# echo "Installing rust demo deps"
-# cd $PROJECT_ROOT/demos/rust
-# cargo build
