955b3f34b5214e2691713622a8aaf7aea921b6ec
Daniel Maricic <daniel@woss.io>
2022-05-23 11:36:23 +0000

    devops and gitpod

--- .devcontainer/install-deps.sh
+++ .devcontainer/install-deps.sh
@@ -8,12 +8,13 @@ sudo apt update && sudo apt install -y direnv
 
 
 echo "Installing the Anagolay CLI"
-sudo sh -c 'curl https://bafybeicjepyfpfqqesjyuqf2zlebww3kzplscmzp4cqjdl5pwqiriadh4u.ipfs.anagolay.network > /usr/local/bin/anagolay && chmod +x /usr/local/bin/anagolay'
+sudo sh -c 'curl https://bafybeif2uvely2ql36lry4j5cgu3fqxlq32ohwdkbgesimyyjsrmn2ze54.ipfs.anagolay.network > /usr/local/bin/anagolay && chmod +x /usr/local/bin/anagolay'
 
 # smoke test
 echo "Anagolay version is $(anagolay --version)"
 
 if [ ! -f "$HOME/.bashrc.d/custom" ]; then
+  mkdir $HOME/.bashrc.d
 	echo "Linking the custom bashrc file"
 	ln -sf $PROJECT_ROOT/.devcontainer/custom_bashrc $HOME/.bashrc.d/custom 
 fi
